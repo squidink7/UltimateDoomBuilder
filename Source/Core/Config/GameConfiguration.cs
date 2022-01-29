squidink7/UltimateDoomBuilder

commit 78ded20481cdf979be9047aff9e872f1ed28bbe1

GZDoom game configuration: added support for distinct upper/middle/lower sidedef brightness levels. Resolves #694

Build/Configurations/Includes/ZDoom_common.cfg
@@ -368,6 +368,9 @@ mapformat_udmf
 	distinctfloorandceilingbrightness = true;
 	distinctwallbrightness = true;
 	
+	// Enabled setting brightness for upper, middle, and lower sidedef indenependtly from each other
+	distinectsidedefpartbrightness = true;
+	
 	// Default nodebuilder configurations
 	defaultsavecompiler = "zdbsp_udmf_normal";
 	defaulttestcompiler = "zdbsp_udmf_fast";

Source/Core/Config/GameConfiguration.cs
@@ -112,6 +112,7 @@ namespace CodeImp.DoomBuilder.Config
 		private readonly bool planeequationsupport;
 		private readonly bool distinctfloorandceilingbrightness;
 		private readonly bool distinctwallbrightness;
+		private readonly bool distinectsidedefpartbrightness;
 
 		// Skills
 		private readonly List<SkillInfo> skills;
@@ -286,6 +287,7 @@ namespace CodeImp.DoomBuilder.Config
 		public bool PlaneEquationSupport { get { return planeequationsupport; } }
 		public bool DistinctFloorAndCeilingBrightness { get { return distinctfloorandceilingbrightness; } }
 		public bool DistinctWallBrightness { get { return distinctwallbrightness; } }
+		public bool DistinectSidedefPartBrightness { get { return distinectsidedefpartbrightness; } }
 
 		// Texture/flat/voxel sources
 		public IDictionary TextureRanges { get { return textureranges; } }
@@ -462,6 +464,7 @@ namespace CodeImp.DoomBuilder.Config
 			planeequationsupport = cfg.ReadSetting("planeequationsupport", false);
 			distinctfloorandceilingbrightness = cfg.ReadSetting("distinctfloorandceilingbrightness", false);
 			distinctwallbrightness = cfg.ReadSetting("distinctwallbrightness", false);
+			distinectsidedefpartbrightness = cfg.ReadSetting("distinectsidedefpartbrightness", false);
 			for (int i = 0; i < Linedef.NUM_ARGS; i++) makedoorargs[i] = cfg.ReadSetting("makedoorarg" + i.ToString(CultureInfo.InvariantCulture), 0);
 
 			//mxd. Update map format flags

Source/Core/Controls/SidedefPartLightControl.cs
@@ -1,0 +1,250 @@
+﻿#region ================== Copyright (c) 2020 Boris Iwanski
+
+/*
+ * This program is free software: you can redistribute it and/or modify
+ *
+ * it under the terms of the GNU General Public License as published by
+ * 
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * 
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.If not, see<http://www.gnu.org/licenses/>.
+ */
+
+#endregion
+
+#region ================== Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using CodeImp.DoomBuilder.Types;
+using CodeImp.DoomBuilder.Map;
+using CodeImp.DoomBuilder.VisualModes;
+using CodeImp.DoomBuilder.Windows;
+
+#endregion
+
+namespace CodeImp.DoomBuilder.Controls
+{
+	public partial class SidedefPartLightControl : UserControl
+	{
+		#region ================== Variables
+
+		private string fieldname;
+		private string fieldabsolutename;
+		private List<Sidedef> sidedefs;
+		private Dictionary<Sidedef, int> originallight;
+		private Dictionary<Sidedef, bool> originalabsolute;
+		private bool preventchanges;
+
+		#endregion
+
+		#region ================== Constructors
+
+		public SidedefPartLightControl()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region ================== Methods
+
+		/// <summary>
+		/// Sets up the control for the specified geometry type.
+		/// </summary>
+		/// <param name="geometrytype">Geometry type to set up the control with</param>
+		public void Setup(VisualGeometryType geometrytype)
+		{
+			fieldname = string.Empty;
+			fieldabsolutename = string.Empty;
+			sidedefs = new List<Sidedef>();
+			originallight = new Dictionary<Sidedef, int>();
+			originalabsolute = new Dictionary<Sidedef, bool>();
+
+			// Do not trigger the events that usually fire when textboxes or checkboxes are changed
+			preventchanges = true;
+
+			switch (geometrytype)
+			{
+				case VisualGeometryType.WALL_UPPER:
+					fieldname = "light_top";
+					fieldabsolutename = "lightabsolute_top";
+					lbLight.Text = "Upper brightness:";
+					break;
+				case VisualGeometryType.WALL_MIDDLE:
+				case VisualGeometryType.WALL_MIDDLE_3D:
+					fieldname = "light_mid";
+					fieldabsolutename = "lightabsolute_mid";
+					lbLight.Text = "Middle brightness:";
+					break;
+				case VisualGeometryType.WALL_LOWER:
+					fieldname = "light_bottom";
+					fieldabsolutename = "lightabsolute_bottom";
+					lbLight.Text = "Lower brightness:";
+					break;
+				default:
+					throw new NotImplementedException("Unsupported geometry type: " + Enum.GetName(typeof(VisualGeometryType), geometrytype));
+			}
+		}
+
+		/// <summary>
+		/// Sets the light value and absolute stats of the control.
+		/// </summary>
+		/// <param name="sidedef">Sidedef to use the values of</param>
+		/// <param name="first">If this is the first sidedef</param>
+		public void SetValues(Sidedef sidedef, bool first)
+		{
+			if (!sidedefs.Contains(sidedef))
+				sidedefs.Add(sidedef);
+
+			originallight[sidedef] = sidedef.Fields.GetValue(fieldname, 0);
+			originalabsolute[sidedef] = sidedef.Fields.GetValue(fieldabsolutename, false);
+
+			string lightvalue = originallight[sidedef].ToString();
+			bool isabsolute = sidedef.Fields.GetValue(fieldabsolutename, false);
+
+			if (first)
+			{
+				light.Text = lightvalue;
+				cbAbsolute.Checked = isabsolute;
+			}
+			else
+			{
+				if (light.Text != lightvalue)
+					light.Text = string.Empty;
+
+				if (cbAbsolute.Checked != isabsolute)
+				{
+					cbAbsolute.ThreeState = true;
+					cbAbsolute.CheckState = CheckState.Indeterminate;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Finalize the control's setup, setting visibility of child controls and enable state
+		/// </summary>
+		public void FinalizeSetup()
+		{
+			reset.Visible = (cbAbsolute.CheckState != CheckState.Unchecked || light.GetResult(0) != 0);
+
+			if (!General.Map.Config.DistinectSidedefPartBrightness)
+			{
+				lbLight.Enabled = false;
+				light.Enabled = false;
+				cbAbsolute.Enabled = false;
+				reset.Enabled = false;
+			}
+
+			preventchanges = false;
+		}
+
+		#endregion
+
+		#region ================== Events
+
+		private void reset_Click(object sender, EventArgs e)
+		{
+			light.Text = "0";
+			cbAbsolute.Checked = false;
+			reset.Visible = false;
+		}
+
+		private void light_WhenTextChanged(object sender, EventArgs e)
+		{
+			if (preventchanges)
+				return;
+
+			((LinedefEditFormUDMF)ParentForm).MakeUndo();
+
+			// Reset the increment step for +++/---
+			light.ResetIncrementStep();
+
+			if (string.IsNullOrEmpty(light.Text))
+			{
+				// Text is empty, use each sidedef's original light value
+				foreach (Sidedef sd in sidedefs)
+				{
+					if (sd == null || sd.IsDisposed)
+						continue;
+
+					UniFields.SetInteger(sd.Fields, fieldname, originallight[sd]);
+				}
+			}
+			else
+			{
+				foreach (Sidedef sd in sidedefs)
+				{
+					if (sd == null || sd.IsDisposed)
+						continue;
+
+					bool absolute = false;
+
+					switch (cbAbsolute.CheckState)
+					{
+						case CheckState.Checked:
+							absolute = true;
+							break;
+						case CheckState.Indeterminate:
+							absolute = sd.Fields.GetValue(fieldabsolutename, false);
+							break;
+					}
+
+					int value = General.Clamp(light.GetResult(originallight[sd]), absolute ? 0 : -255, 255);
+					UniFields.SetInteger(sd.Fields, fieldname, value);
+				}
+			}
+
+			reset.Visible = (cbAbsolute.CheckState != CheckState.Unchecked || light.Text != "0");
+			General.Map.IsChanged = true;
+
+			((LinedefEditFormUDMF)ParentForm).ValuesChangedExternal();
+		}
+
+		private void cbAbsolute_CheckedChanged(object sender, EventArgs e)
+		{
+			if (preventchanges)
+				return;
+
+			((LinedefEditFormUDMF)ParentForm).MakeUndo();
+
+			if (cbAbsolute.Checked)
+			{
+				foreach(Sidedef sd in sidedefs)
+				{
+					sd.Fields[fieldabsolutename] = new UniValue(UniversalType.Boolean, true);
+				}
+			}
+			else if(cbAbsolute.CheckState == CheckState.Indeterminate)
+			{
+				foreach(Sidedef sd in sidedefs)
+				{
+					if (originalabsolute[sd])
+						sd.Fields[fieldabsolutename] = new UniValue(UniversalType.Boolean, true);
+					else if (sd.Fields.ContainsKey(fieldabsolutename))
+						sd.Fields.Remove(fieldabsolutename);
+				}
+			}
+			else
+			{
+				foreach (Sidedef sd in sidedefs)
+					if (sd.Fields.ContainsKey(fieldabsolutename))
+						sd.Fields.Remove(fieldabsolutename);
+			}
+
+			((LinedefEditFormUDMF)ParentForm).ValuesChangedExternal();
+		}
+
+		#endregion
+	}
+}

Source/Plugins/BuilderModes/VisualModes/BaseVisualGeometrySidedef.cs
@@ -522,10 +522,48 @@ namespace CodeImp.DoomBuilder.BuilderModes
 		//mxd
 		protected void GetLightValue(out int lightvalue, out bool lightabsolute)
 		{
-			lightabsolute = Sidedef.Fields.GetValue("lightabsolute", false);
+			string partstr = string.Empty, partstrabs = string.Empty;
+
+			if (General.Map.Config.DistinectSidedefPartBrightness)
+			{
+				switch (geometrytype)
+				{
+					case VisualGeometryType.WALL_UPPER:
+						partstr = "light_top";
+						partstrabs = "lightabsolute_top";
+						break;
+					case VisualGeometryType.WALL_LOWER:
+						partstr = "light_bottom";
+						partstrabs = "lightabsolute_bottom";
+						break;
+					case VisualGeometryType.WALL_MIDDLE:
+					case VisualGeometryType.WALL_MIDDLE_3D:
+						partstr = "light_mid";
+						partstrabs = "lightabsolute_mid";
+						break;
+				}
+			}
+
+			bool lightglobalabsolute = Sidedef.Fields.GetValue("lightabsolute", false);
+			bool lightpartabsolute = General.Map.Config.DistinectSidedefPartBrightness ? Sidedef.Fields.GetValue(partstrabs, false) : false;
+			lightabsolute = lightglobalabsolute || lightpartabsolute;
 			bool affectedbyfog = General.Map.Data.MapInfo.HasFadeColor || (Sector.Sector.HasSkyCeiling && General.Map.Data.MapInfo.HasOutsideFogColor) || Sector.Sector.Fields.ContainsKey("fadecolor");
 			bool ignorelight = affectedbyfog && !Sidedef.IsFlagSet("lightfog") && !lightabsolute;
 			lightvalue = ignorelight ? 0 : Sidedef.Fields.GetValue("light", 0); //mxd
+
+			if(ignorelight)
+			{
+				lightvalue = 0;
+			}
+			else
+			{
+				// Absolute value of upper/middle/lower always has precedence
+				if(lightpartabsolute)
+					lightvalue = Sidedef.Fields.GetValue(partstr, 0);
+				else
+					lightvalue = Sidedef.Fields.GetValue("light", 0) + (General.Map.Config.DistinectSidedefPartBrightness ? Sidedef.Fields.GetValue(partstr, 0) : 0);
+			}
+
 			if(ignorelight) lightabsolute = false;
 		}
 
@@ -1550,10 +1588,19 @@ namespace CodeImp.DoomBuilder.BuilderModes
 		public virtual void OnChangeTargetBrightness(bool up)
 		{
 			//mxd. Change UDMF wall light?
-			if(General.Map.UDMF && General.Map.Config.DistinctWallBrightness)
+			if(General.Map.UDMF && (General.Map.Config.DistinctWallBrightness || General.Map.Config.DistinectSidedefPartBrightness))
 			{
-				int light = Sidedef.Fields.GetValue("light", 0);
-				bool absolute = Sidedef.Fields.GetValue("lightabsolute", false);
+				string fieldname = "light";
+				string fieldabsolutename = "lightabsolute";
+
+				if(General.Map.Config.DistinectSidedefPartBrightness)
+				{
+					fieldname += "_" + partname;
+					fieldabsolutename += "_" + partname;
+				}
+
+				int light = Sidedef.Fields.GetValue(fieldname, 0);
+				bool absolute = Sidedef.Fields.GetValue(fieldabsolutename, false);
 				int newlight;
 
 				if(up)
@@ -1568,7 +1615,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 				Sidedef.Fields.BeforeFieldsChange();
 
 				// Apply changes
-				UniFields.SetInteger(Sidedef.Fields, "light", newlight, (absolute ? int.MinValue : 0));
+				UniFields.SetInteger(Sidedef.Fields, fieldname, newlight, (absolute ? int.MinValue : 0));
 				Tools.UpdateLightFogFlag(Sidedef);
 				mode.SetActionResult("Changed wall brightness to " + newlight + ".");
 

Build/Configurations/Includes/ZDoom_misc.cfg
@@ -467,7 +467,43 @@ universalfields
 		{
 			type = 1;
 			default = 1.0;
-		}		
+		}
+		
+		light_top
+		{
+			type = 0;
+			default = 0;
+		}
+		
+		lightabsolute_top
+		{
+			type = 3;
+			default = false;
+		}
+		
+		light_mid
+		{
+			type = 0;
+			default = 0;
+		}
+		
+		lightabsolute_mid
+		{
+			type = 3;
+			default = false;
+		}
+		
+		light_bottom
+		{
+			type = 0;
+			default = 0;
+		}
+		
+		lightabsolute_bottom
+		{
+			type = 3;
+			default = false;
+		}
 	}
 	
 	thing

Source/Core/Builder.csproj
@@ -191,6 +191,12 @@
       <SubType>Component</SubType>
     </Compile>
     <Compile Include="Controls\Scripting\TextEditorControl.cs" />
+    <Compile Include="Controls\SidedefPartLightControl.cs">
+      <SubType>UserControl</SubType>
+    </Compile>
+    <Compile Include="Controls\SidedefPartLightControl.Designer.cs">
+      <DependentUpon>SidedefPartLightControl.cs</DependentUpon>
+    </Compile>
     <Compile Include="Controls\TransparentTrackBar.cs">
       <SubType>Component</SubType>
     </Compile>
@@ -668,6 +674,9 @@
     </EmbeddedResource>
     <EmbeddedResource Include="Controls\PairedFloatControl.resx">
       <DependentUpon>PairedFloatControl.cs</DependentUpon>
+    </EmbeddedResource>
+    <EmbeddedResource Include="Controls\SidedefPartLightControl.resx">
+      <DependentUpon>SidedefPartLightControl.cs</DependentUpon>
     </EmbeddedResource>
     <EmbeddedResource Include="Windows\PreAndPostCommandsForm.resx">
       <DependentUpon>PreAndPostCommandsForm.cs</DependentUpon>

Source/Core/BuilderMono.csproj
@@ -188,6 +188,12 @@
       <SubType>Component</SubType>
     </Compile>
     <Compile Include="Controls\Scripting\TextEditorControl.cs" />
+    <Compile Include="Controls\SidedefPartLightControl.cs">
+      <SubType>UserControl</SubType>
+    </Compile>
+    <Compile Include="Controls\SidedefPartLightControl.Designer.cs">
+      <DependentUpon>SidedefPartLightControl.cs</DependentUpon>
+    </Compile>
     <Compile Include="Controls\TransparentTrackBar.cs">
       <SubType>Component</SubType>
     </Compile>
@@ -660,6 +666,9 @@
     </EmbeddedResource>
     <EmbeddedResource Include="Controls\PairedFloatControl.resx">
       <DependentUpon>PairedFloatControl.cs</DependentUpon>
+    </EmbeddedResource>
+    <EmbeddedResource Include="Controls\SidedefPartLightControl.resx">
+      <DependentUpon>SidedefPartLightControl.cs</DependentUpon>
     </EmbeddedResource>
     <EmbeddedResource Include="Windows\PreAndPostCommandsForm.resx">
       <DependentUpon>PreAndPostCommandsForm.cs</DependentUpon>

Source/Core/Controls/SidedefPartLightControl.Designer.cs
@@ -1,0 +1,111 @@
+﻿namespace CodeImp.DoomBuilder.Controls
+{
+	partial class SidedefPartLightControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lbLight = new System.Windows.Forms.Label();
+			this.cbAbsolute = new System.Windows.Forms.CheckBox();
+			this.reset = new System.Windows.Forms.Button();
+			this.light = new CodeImp.DoomBuilder.Controls.ButtonsNumericTextbox();
+			this.SuspendLayout();
+			// 
+			// lbLight
+			// 
+			this.lbLight.Location = new System.Drawing.Point(3, 6);
+			this.lbLight.Name = "lbLight";
+			this.lbLight.Size = new System.Drawing.Size(92, 14);
+			this.lbLight.TabIndex = 29;
+			this.lbLight.Tag = "";
+			this.lbLight.Text = "Brightness:";
+			this.lbLight.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// cbAbsolute
+			// 
+			this.cbAbsolute.AutoSize = true;
+			this.cbAbsolute.Location = new System.Drawing.Point(167, 7);
+			this.cbAbsolute.Name = "cbAbsolute";
+			this.cbAbsolute.Size = new System.Drawing.Size(67, 17);
+			this.cbAbsolute.TabIndex = 31;
+			this.cbAbsolute.Tag = "lightabsolute";
+			this.cbAbsolute.Text = "Absolute";
+			this.cbAbsolute.UseVisualStyleBackColor = true;
+			this.cbAbsolute.CheckedChanged += new System.EventHandler(this.cbAbsolute_CheckedChanged);
+			// 
+			// reset
+			// 
+			this.reset.Image = global::CodeImp.DoomBuilder.Properties.Resources.Reset;
+			this.reset.Location = new System.Drawing.Point(236, 3);
+			this.reset.Name = "reset";
+			this.reset.Size = new System.Drawing.Size(23, 23);
+			this.reset.TabIndex = 32;
+			this.reset.UseVisualStyleBackColor = true;
+			this.reset.Click += new System.EventHandler(this.reset_Click);
+			// 
+			// light
+			// 
+			this.light.AllowDecimal = false;
+			this.light.AllowExpressions = false;
+			this.light.AllowNegative = true;
+			this.light.AllowRelative = true;
+			this.light.ButtonStep = 16;
+			this.light.ButtonStepBig = 32F;
+			this.light.ButtonStepFloat = 1F;
+			this.light.ButtonStepSmall = 1F;
+			this.light.ButtonStepsUseModifierKeys = true;
+			this.light.ButtonStepsWrapAround = false;
+			this.light.Location = new System.Drawing.Point(99, 2);
+			this.light.Name = "light";
+			this.light.Size = new System.Drawing.Size(62, 24);
+			this.light.StepValues = null;
+			this.light.TabIndex = 30;
+			this.light.Tag = "";
+			this.light.WhenTextChanged += new System.EventHandler(this.light_WhenTextChanged);
+			// 
+			// SidedefPartLightControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.reset);
+			this.Controls.Add(this.light);
+			this.Controls.Add(this.lbLight);
+			this.Controls.Add(this.cbAbsolute);
+			this.Name = "SidedefPartLightControl";
+			this.Size = new System.Drawing.Size(262, 29);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button reset;
+		private ButtonsNumericTextbox light;
+		private System.Windows.Forms.Label lbLight;
+		private System.Windows.Forms.CheckBox cbAbsolute;
+	}
+}

commit 6f8d617347d842e6e7e8de2864bc09244de12a2c

Fixed typos introduced in 78ded20481cdf979be9047aff9e872f1ed28bbe1.

Build/Configurations/Includes/ZDoom_common.cfg
@@ -368,8 +368,8 @@ mapformat_udmf
 	distinctfloorandceilingbrightness = true;
 	distinctwallbrightness = true;
 	
-	// Enabled setting brightness for upper, middle, and lower sidedef indenependtly from each other
-	distinectsidedefpartbrightness = true;
+	// Enabled setting brightness for upper, middle, and lower sidedef independently from each other
+	distinctsidedefpartbrightness = true;
 	
 	// Default nodebuilder configurations
 	defaultsavecompiler = "zdbsp_udmf_normal";

Source/Core/Config/GameConfiguration.cs
@@ -112,7 +112,7 @@ namespace CodeImp.DoomBuilder.Config
 		private readonly bool planeequationsupport;
 		private readonly bool distinctfloorandceilingbrightness;
 		private readonly bool distinctwallbrightness;
-		private readonly bool distinectsidedefpartbrightness;
+		private readonly bool distinctsidedefpartbrightness;
 
 		// Skills
 		private readonly List<SkillInfo> skills;
@@ -287,7 +287,7 @@ namespace CodeImp.DoomBuilder.Config
 		public bool PlaneEquationSupport { get { return planeequationsupport; } }
 		public bool DistinctFloorAndCeilingBrightness { get { return distinctfloorandceilingbrightness; } }
 		public bool DistinctWallBrightness { get { return distinctwallbrightness; } }
-		public bool DistinectSidedefPartBrightness { get { return distinectsidedefpartbrightness; } }
+		public bool DistinctSidedefPartBrightness { get { return distinctsidedefpartbrightness; } }
 
 		// Texture/flat/voxel sources
 		public IDictionary TextureRanges { get { return textureranges; } }
@@ -464,7 +464,7 @@ namespace CodeImp.DoomBuilder.Config
 			planeequationsupport = cfg.ReadSetting("planeequationsupport", false);
 			distinctfloorandceilingbrightness = cfg.ReadSetting("distinctfloorandceilingbrightness", false);
 			distinctwallbrightness = cfg.ReadSetting("distinctwallbrightness", false);
-			distinectsidedefpartbrightness = cfg.ReadSetting("distinectsidedefpartbrightness", false);
+			distinctsidedefpartbrightness = cfg.ReadSetting("distinctsidedefpartbrightness", false);
 			for (int i = 0; i < Linedef.NUM_ARGS; i++) makedoorargs[i] = cfg.ReadSetting("makedoorarg" + i.ToString(CultureInfo.InvariantCulture), 0);
 
 			//mxd. Update map format flags

Source/Core/Controls/SidedefPartLightControl.cs
@@ -138,7 +138,7 @@ namespace CodeImp.DoomBuilder.Controls
 		{
 			reset.Visible = (cbAbsolute.CheckState != CheckState.Unchecked || light.GetResult(0) != 0);
 
-			if (!General.Map.Config.DistinectSidedefPartBrightness)
+			if (!General.Map.Config.DistinctSidedefPartBrightness)
 			{
 				lbLight.Enabled = false;
 				light.Enabled = false;

Source/Plugins/BuilderModes/VisualModes/BaseVisualGeometrySidedef.cs
@@ -524,7 +524,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 		{
 			string partstr = string.Empty, partstrabs = string.Empty;
 
-			if (General.Map.Config.DistinectSidedefPartBrightness)
+			if (General.Map.Config.DistinctSidedefPartBrightness)
 			{
 				switch (geometrytype)
 				{
@@ -545,7 +545,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 			}
 
 			bool lightglobalabsolute = Sidedef.Fields.GetValue("lightabsolute", false);
-			bool lightpartabsolute = General.Map.Config.DistinectSidedefPartBrightness ? Sidedef.Fields.GetValue(partstrabs, false) : false;
+			bool lightpartabsolute = General.Map.Config.DistinctSidedefPartBrightness ? Sidedef.Fields.GetValue(partstrabs, false) : false;
 			lightabsolute = lightglobalabsolute || lightpartabsolute;
 			bool affectedbyfog = General.Map.Data.MapInfo.HasFadeColor || (Sector.Sector.HasSkyCeiling && General.Map.Data.MapInfo.HasOutsideFogColor) || Sector.Sector.Fields.ContainsKey("fadecolor");
 			bool ignorelight = affectedbyfog && !Sidedef.IsFlagSet("lightfog") && !lightabsolute;
@@ -561,7 +561,7 @@ namespace CodeImp.DoomBuilder.BuilderModes
 				if(lightpartabsolute)
 					lightvalue = Sidedef.Fields.GetValue(partstr, 0);
 				else
-					lightvalue = Sidedef.Fields.GetValue("light", 0) + (General.Map.Config.DistinectSidedefPartBrightness ? Sidedef.Fields.GetValue(partstr, 0) : 0);
+					lightvalue = Sidedef.Fields.GetValue("light", 0) + (General.Map.Config.DistinctSidedefPartBrightness ? Sidedef.Fields.GetValue(partstr, 0) : 0);
 			}
 
 			if(ignorelight) lightabsolute = false;
@@ -1588,12 +1588,12 @@ namespace CodeImp.DoomBuilder.BuilderModes
 		public virtual void OnChangeTargetBrightness(bool up)
 		{
 			//mxd. Change UDMF wall light?
-			if(General.Map.UDMF && (General.Map.Config.DistinctWallBrightness || General.Map.Config.DistinectSidedefPartBrightness))
+			if(General.Map.UDMF && (General.Map.Config.DistinctWallBrightness || General.Map.Config.DistinctSidedefPartBrightness))
 			{
 				string fieldname = "light";
 				string fieldabsolutename = "lightabsolute";
 
-				if(General.Map.Config.DistinectSidedefPartBrightness)
+				if(General.Map.Config.DistinctSidedefPartBrightness)
 				{
 					fieldname += "_" + partname;
 					fieldabsolutename += "_" + partname;