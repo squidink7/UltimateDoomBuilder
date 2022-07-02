59757c3be509a71e50ce57e031f298469baca75e
biwa <6475593+biwa@users.noreply.github.com>
2022-07-02 19:25:10 +0200

    Sound Environment Mode: fixed a crash that happened when exiting Draw Lines Mode with the "edit" action and returning to Sound Environment Mode. Fixes #755

--- Source/Plugins/SoundPropagationMode/SoundEnvironmentMode.cs
+++ Source/Plugins/SoundPropagationMode/SoundEnvironmentMode.cs
@@ -219,9 +219,20 @@ namespace CodeImp.DoomBuilder.SoundPropagationMode
 			}
 		}
 
+		protected override void OnEditBegin()
+		{
+			base.OnEditBegin();
+
+			editpressed = true;
+		}
+
 		//mxd. Show Reverb selector dialog or add a new sound environment thing
 		protected override void OnEditEnd()
 		{
+			// Edit pressed in this mode?
+			if (!editpressed)
+				return;
+
 			if(highlightedthing != null)
 			{
 				ReverbsPickerForm form = new ReverbsPickerForm(highlightedthing);
